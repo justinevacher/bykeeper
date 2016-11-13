11d668278a2cd157bbb27beba090d9d579766c01
Greg <Greg>
2016-11-13 15:25:55 +0100

    fix burger.js

--- js/burger.js
+++ js/burger.js
@@ -4,9 +4,21 @@
   var burgerContain = document.getElementsByClassName('b-container')[0];
   var burgerNav = document.getElementsByClassName('b-nav')[0];
 
+  
+
   burgerMenu.addEventListener('click', function toggleClasses() {
     [body, burgerContain, burgerNav].forEach(function (el) {
-      el.classList.toggle('open');
+      if (typeof burgerEstOuvert == "undefined") { // Test si variable existe => utile lors de la première initialisation
+  		burgerEstOuvert = true; // Au premier clic, on ouvre le burger
+	} else {
+		if(burgerEstOuvert == true){
+			el.classList.toggle('close'); // Le burger était ouvert, on le ferme
+			burgerEstOuvert = false;
+		}else{
+			el.classList.toggle('open'); // Le burger était fermé, on l'ouvre
+			burgerEstOuvert = true;
+	}
+}
     });
   }, false);
 })();
