d02587c4f308624953789a3200d7bae47b6016c8
Greg <Greg>
2016-12-27 15:38:34 +0100

    Ajout formulaire symfony pour les donnees perso utilisateur

--- src/KeeperBundle/Controller/DefaultController.php
+++ src/KeeperBundle/Controller/DefaultController.php
@@ -4,6 +4,11 @@ namespace KeeperBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use KeeperBundle\Entity\Utilisateur;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class DefaultController extends Controller
 {
@@ -50,9 +55,35 @@ class DefaultController extends Controller
     /**
      * @Route("/donnees-perso", name="donnees_perso")
      */
-    public function donneesPersoAction()
+    public function donneesPersoAction(Request $request)
     {
-        return $this->render('KeeperBundle:Default:donnees-perso.html.twig');
+
+        $userManager = $this->get('fos_user.user_manager');
+        $userId = $this->getUser()->getId();
+        $user = $userManager->findUserBy(array('id' => $userId));
+        $numBoitier = $user->getBoitier()->getNumBoitier();
+
+        $form = $this->createFormBuilder($user)
+        ->add('username', TextType::class, array('label' => 'Nom d\'Utilisateur',
+                                                'disabled' => 'true'))
+        ->add('numTel', TextType::class, array('label' => 'Numéro de téléphone portable'))
+        ->add('email', EmailType::class)
+        ->add('submit', SubmitType::class, array('label' => 'Enregistrer'))
+        ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+        }
+
+        return $this->render('KeeperBundle:Default:donnees-perso.html.twig', array(
+            'form' => $form->createView(),
+            'numBoitier' => $numBoitier
+            ));
     }
 
     /**
