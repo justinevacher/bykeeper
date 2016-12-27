0c9920e4bc3a5230b85b02aae9b0870d69ad19ca
Greg <Greg>
2016-12-27 18:24:59 +0100

    Ajout formulaire pour parametrage notifications

--- src/KeeperBundle/Controller/DefaultController.php
+++ src/KeeperBundle/Controller/DefaultController.php
@@ -6,9 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
 use KeeperBundle\Entity\Utilisateur;
+use KeeperBundle\Entity\Boitier;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+
 
 class DefaultController extends Controller
 {
@@ -31,15 +34,39 @@ class DefaultController extends Controller
     /**
      * @Route("/parametrage-alarme", name="parametrage_alarme")
      */
-    public function parametrageAlarmeAction()
+    public function parametrageAlarmeAction(Request $request)
     {
-        return $this->render('KeeperBundle:Default:parametrage-alarme.html.twig');
+        $userManager = $this->get('fos_user.user_manager');
+        $userId = $this->getUser()->getId();
+        $user = $userManager->findUserBy(array('id' => $userId));
+
+        $form = $this->createFormBuilder($user)
+        ->add('notifSMS', CheckboxType::class, array('label' => 'Par SMS',
+           'required' =>false))
+        ->add('notifEmail', CheckboxType::class, array('label' => 'Par MAIL',
+            'required' =>false))
+        ->add('notifZonesRisques', CheckboxType::class, array('label'=> "Lorsque j'active mon alarme dans une zone à risques",'required' =>false))
+        ->add('submit', SubmitType::class, array('label' => 'Enregistrer'))
+        ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+        }
+
+        return $this->render('KeeperBundle:Default:parametrage-alarme.html.twig', array(
+            'form' => $form->createView(),
+            ));
     }
 
     /**
      * @Route("/reglage-capteur", name="reglage_capteur")
      */
-    public function reglageCapteurAction()
+    public function reglageCapteurAction(Request $request)
     {
         return $this->render('KeeperBundle:Default:reglage-capteur.html.twig');
     }
@@ -65,7 +92,7 @@ class DefaultController extends Controller
 
         $form = $this->createFormBuilder($user)
         ->add('username', TextType::class, array('label' => 'Nom d\'Utilisateur',
-                                                'disabled' => 'true'))
+            'disabled' => 'true'))
         ->add('numTel', TextType::class, array('label' => 'Numéro de téléphone portable'))
         ->add('email', EmailType::class)
         ->add('submit', SubmitType::class, array('label' => 'Enregistrer'))
